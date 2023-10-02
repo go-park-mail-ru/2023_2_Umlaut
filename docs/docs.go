f01bf65d85120d9318faed2f64970ac23ee734a5
Maxim Sikanov <max.4251474@yandex.ru>
2023-10-02 14:59:10 +0300

    fix-cors

--- docs/docs.go
+++ docs/docs.go
@@ -210,7 +210,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8000",
+	Host:             "37.139.32.76:8000",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Umlaut API",
