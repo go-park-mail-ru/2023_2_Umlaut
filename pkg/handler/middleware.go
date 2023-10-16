d9077faef5e5ae82d22653168869e68c9a2fd302
Maxim Sikanov <max.4251474@yandex.ru>
2023-10-16 12:58:27 +0300

    middleware

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -20,21 +20,21 @@ func corsMiddleware(next http.Handler) http.Handler {
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, req)
-		logrus.Printf("%s %s %s", req.Method, req.RequestURI, time.Since(start))
+		next.ServeHTTP(w, r)
+		logrus.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
 	})
 }
 
 func panicRecoveryMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
 				newErrorResponse(w, http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError))
 				logrus.Println(string(debug.Stack()))
 			}
 		}()
-		next.ServeHTTP(w, req)
+		next.ServeHTTP(w, r)
 	})
 }
