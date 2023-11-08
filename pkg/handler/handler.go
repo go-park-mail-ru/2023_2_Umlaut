3ed2d18c8ac518181e36b82c534437948cb97a62
Stoma Fedor <fedorstoma2@gmail.com>
2023-11-08 16:47:18 +0300

    update swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -22,7 +22,7 @@ func NewHandler(services *service.Service, ctx context.Context) *Handler {
 func (h *Handler) InitRoutes() http.Handler {
 	r := mux.NewRouter()
 	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8000/swagger/doc.json"),
+		httpSwagger.URL("http://umlaut-bmstu.me:8000/swagger/doc.json"),
 	))
 
 	authRouter := r.PathPrefix("/auth").Subrouter()
