320a14163c7292009972fb071fcf85223207282b
Stoma Fedor <fedorstoma2@gmail.com>
2023-11-19 23:43:40 +0300

    add https adress))

--- static/constants.go
+++ static/constants.go
@@ -1,7 +1,7 @@
 package static
 
 var (
-	Host           = "http://umlaut-bmstu.me"
+	Host           = "https://umlaut-bmstu.me"
 	MessageDbField = "id, dialog_id, sender_id, message_text, created_at"
 	LikeDbField    = "liked_by_user_id, liked_to_user_id"
 	UserDbField    = "id, name, mail, password_hash, salt, user_gender, prefer_gender, description, looking, image_paths, education, hobbies, birthday, online, tags"
