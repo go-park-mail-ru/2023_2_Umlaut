c1937f6f206417d7b333fdcf657810e6e0581bc1
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-19 13:35:48 +0300

    fix-hendle

--- pkg/handler/dialog.go
+++ pkg/handler/dialog.go
@@ -11,7 +11,7 @@ import (
 // @Produce  json
 // @Success 200 {object} ClientResponseDto[[]model.Dialog]
 // @Failure 401,500 {object} ClientResponseDto[string]
-// @Router /api/v1/dialog [get]
+// @Router /api/v1/dialogs [get]
 func (h *Handler) getDialogs(w http.ResponseWriter, r *http.Request) {
 	userId := r.Context().Value(keyUserID).(int)
 
