0990d3518dd252dd359aa6cb407daaaadf5a6b5c
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-24 20:59:47 +0300

    Deploy

--- static/constants.go
+++ static/constants.go
@@ -1,7 +1,7 @@
 package static
 
 var (
-	Host           = "http://localhost"
+	Host           = "http://umlaut-bmstu.me"
 	MessageDbField = "id, dialog_id, sender_id, message_text, created_at"
 	LikeDbField    = "liked_by_user_id, liked_to_user_id"
 	UserDbField    = "id, name, mail, password_hash, salt, user_gender, prefer_gender, description, looking, image_paths, education, hobbies, birthday, online, tags"
