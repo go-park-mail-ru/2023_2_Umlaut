8fdcc048349efe9dcbcf59b179bd2070dd1bbc59
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-25 12:47:03 +0300

    model stat

--- model/statistic.go
+++ model/statistic.go
@@ -1,4 +1,21 @@
 package model
 
+import "time"
+
 type Statistic struct {
+	Id         int        `json:"id" db:"id"`
+	UserId     int        `json:"user_id" db:"user_id"`
+	Rating     *int       `json:"rating" db:"rating"`
+	Liked      *string    `json:"liked" db:"liked"`
+	NeedFix    *string    `json:"need_fix" db:"need_fix"`
+	CommentFix *string    `json:"comment_fix" db:"comment_fix"`
+	Comment    *string    `json:"comment" db:"comment"`
+	CreatedAt  *time.Time `json:"created_at" db:"created_at"`
+}
+
+type Recommendation struct {
+	Id        int        `json:"id" db:"id"`
+	UserId    int        `json:"user_id" db:"user_id"`
+	Recommend *int       `json:"recommend" db:"recommend"`
+	CreatedAt *time.Time `json:"created_at" db:"created_at"`
 }
