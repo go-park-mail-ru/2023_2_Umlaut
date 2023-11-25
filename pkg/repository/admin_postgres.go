6c22e3e74a78133bcf681c46cc508e602cc97549
Stoma Fedor <fedorstoma2@gmail.com>
2023-11-25 13:54:30 +0300

    add create stat repo

--- pkg/repository/admin_postgres.go
+++ pkg/repository/admin_postgres.go
@@ -40,6 +40,24 @@ func (r *AdminPostgres) GetAdmin(ctx context.Context, mail string) (model.Admin,
 	return admin, err
 }
 
+func (r *AdminPostgres) CreateStatistic(ctx context.Context, stat model.Statistic) (int, error) {
+	var id int
+	query, args, err := psql.Insert(statisticTable).
+		Columns("user_id", "rating", "liked", "need_fix", "comment_fix", "comment", "created_at").
+		Values(stat.UserId, stat.Rating, stat.Liked, stat.NeedFix, stat.CommentFix, stat.Comment, stat.CreatedAt).
+		ToSql()
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to create statistic. err: %w", err)
+	}
+
+	query += " RETURNING id"
+	row := r.db.QueryRow(ctx, query, args...)
+	err = row.Scan(&id)
+
+	return id, err
+}
+
 func scanAdmin(row pgx.Row, admin *model.Admin) error {
 	err := row.Scan(
 		&admin.Id,
