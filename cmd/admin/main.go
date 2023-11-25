61a00fe20a6c86e715a9a0446dcb689ac94d62f7
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-25 11:53:48 +0300

    Admin microservice

---
+++ cmd/admin/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"github.com/go-park-mail-ru/2023_2_Umlaut/pkg/microservices/admin/proto"
+	"log"
+	"net"
+
+	utils "github.com/go-park-mail-ru/2023_2_Umlaut/cmd"
+	"github.com/go-park-mail-ru/2023_2_Umlaut/pkg/microservices/admin/server"
+	"github.com/go-park-mail-ru/2023_2_Umlaut/pkg/repository"
+	"github.com/go-park-mail-ru/2023_2_Umlaut/pkg/service"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ctx := context.Background()
+
+	db, err := utils.InitPostgres(ctx)
+	if err != nil {
+		log.Fatalf("failed to initialize Postgres: %s", err.Error())
+	}
+
+	sessionStore, err := utils.InitRedis()
+	if err != nil {
+		log.Fatalf("failed to initialize redisDb: %s", err.Error())
+	}
+	defer sessionStore.Close()
+
+	adminService := service.NewAdminService(repository.NewAdminPostgres(db))
+
+	adminServer := server.NewAdminServer(adminService)
+	viper.GetString("admin.port")
+	listen, err := net.Listen("tcp", ":"+viper.GetString("admin.port"))
+	if err != nil {
+		log.Fatalf("Cannot listen port: %s. Err: %s", viper.GetString("admin.port"), err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+
+	proto.RegisterAdminServer(grpcServer)
+
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		log.Fatalf("Cannot listen port: %s. Err: %s", viper.GetString("admin.port"), err.Error())
+	}
+}
