a7fa0afde06bd393f8a9ee894c183ecdbd02207c
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-25 17:19:14 +0300

    fix

--- static/constants.go
+++ static/constants.go
@@ -2,12 +2,12 @@ package static
 
 var (
 	Host     = Protocol + Adress
-	Protocol = "http://"
-	Adress   = "localhost"
+	Protocol = "https://"
+	Adress   = "umlaut-bmstu.me"
 
-	MessageDbField = "id, dialog_id, sender_id, message_text, created_at"
-	LikeDbField    = "liked_by_user_id, liked_to_user_id"
-	UserDbField    = "id, name, mail, password_hash, salt, user_gender, prefer_gender, description, looking, image_paths, education, hobbies, birthday, online, tags"
-	AdminDbField = "id, name, mail, password_hash, salt"
+	MessageDbField  = "id, dialog_id, sender_id, message_text, created_at"
+	LikeDbField     = "liked_by_user_id, liked_to_user_id"
+	UserDbField     = "id, name, mail, password_hash, salt, user_gender, prefer_gender, description, looking, image_paths, education, hobbies, birthday, online, tags"
+	AdminDbField    = "id, name, mail, password_hash, salt"
 	FeedbackDbField = "id, user_id, rating, liked, need_fix, comment_fix, comment, show, created_at"
 )
