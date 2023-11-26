5c6c9e3c3aafbca0b7ceeae64d5540c471c65dce
Stoma Fedor <fedorstoma2@gmail.com>
2023-11-27 00:26:41 +0300

    try to fix swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -44,7 +44,7 @@ func NewHandler(
 func (h *Handler) InitRoutes() http.Handler {
 	r := mux.NewRouter()
 	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
-		httpSwagger.URL(fmt.Sprintf("http://%s:8000/swagger/doc.json", static.Adress)),
+		httpSwagger.URL(fmt.Sprintf("https://%s:8000/swagger/doc.json", static.Adress)),
 	))
 
 	authRouter := r.PathPrefix("/api/v1/auth").Subrouter()
