063fe1055395b87c9183c76361efd6995f34a34e
Maxim Sikanov <max.4251474@yandex.ru>
2023-11-29 01:06:06 +0300

    fix-metric

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -121,7 +121,6 @@ func (h *Handler) loggingMiddleware(next http.Handler) http.Handler {
 		re := regexp.MustCompile(`\d+`)
 		path = strings.Split(re.ReplaceAllString(path, "1"), "?")[0]
 
-		h.metrics.RequestCounter.Inc()
 		h.metrics.Hits.WithLabelValues(strconv.Itoa(requestInfo.Status), path, method).Inc()
 		h.metrics.Duration.WithLabelValues(strconv.Itoa(requestInfo.Status), path, method).Observe(timing.Seconds())
 	})
--- pkg/monitoring/monitoring.go
+++ pkg/monitoring/monitoring.go
@@ -7,23 +7,17 @@ import (
 )
 
 type PrometheusMetrics struct {
-	RequestCounter prometheus.Counter
-	Hits           *prometheus.CounterVec
-	Duration       *prometheus.HistogramVec
+	Hits     *prometheus.CounterVec
+	Duration *prometheus.HistogramVec
 }
 
 func RegisterMonitoring(router *mux.Router) *PrometheusMetrics {
 	var metrics = new(PrometheusMetrics)
-	metrics.RequestCounter = prometheus.NewCounter(prometheus.CounterOpts{
-		Namespace: "api",
-		Name:      "requests_total",
-		Help:      "Total number of requests.",
-	})
 	metrics.Hits = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: "api",
 		Name:      "hits_total",
 		Help:      "Total number of hits.",
-	}, []string{"status", "path", "method"})
+	}, []string{"path", "method"})
 	metrics.Duration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: "api",
 		Name:      "request_duration_seconds",
@@ -31,7 +25,7 @@ func RegisterMonitoring(router *mux.Router) *PrometheusMetrics {
 		Buckets:   prometheus.DefBuckets,
 	}, []string{"status", "path", "method"})
 
-	prometheus.MustRegister(metrics.RequestCounter, metrics.Hits, metrics.Duration)
+	prometheus.MustRegister(metrics.Hits, metrics.Duration)
 
 	router.Path("/metrics").Handler(promhttp.Handler())
 
