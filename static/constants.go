2e3bc10f48f3d17d24c31d95e40d191d7d1bdb0d
vm <max>
2023-11-28 11:48:20 +0000

    fix

--- static/constants.go
+++ static/constants.go
@@ -4,7 +4,7 @@ var (
 	Host = "https://umlaut-bmstu.me"
 	// Host = "http://localhost:8000"
 
-	MessageDbField       = "id, dialog_id, sender_id, message_text, created_at"
+	MessageDbField       = "id, dialog_id, sender_id, message_text, is_read, created_at"
 	LikeDbField          = "liked_by_user_id, liked_to_user_id"
 	UserDbField          = "id, name, mail, password_hash, salt, user_gender, prefer_gender, description, looking, image_paths, education, hobbies, birthday, banned, online, tags"
 	AdminDbField         = "id, mail, password_hash, salt"
